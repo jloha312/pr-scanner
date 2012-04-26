53bdd8982acad58625abc6f6225bd1c33e75c73a
John Lohavichan <john.lohavichan@gmail.com>
2012-04-26 11:20:12 -0400

    Added all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrScanner::Application.config.secret_token = '3ba443b6ca5923025714c1a50b4eb318cb461198dde8a65a13430e6077dfaf49a25399882808bad74fe6cd6e9dd355ff8ed3824dc6d09ce0938113b35b8f331b'
